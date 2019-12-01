2bfb8956a684de827074e323e63d0bff262c1e58
Secret Headquarters <secretheadquarters2019@gmail.com>
2019-12-01 15:39:39 +0000

    Added setup scripts

---
+++ setup/setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo Update the password for the current user
+passwd
+
+CURRENT_HOSTNAME=`cat /etc/hostname | tr -d " \t\n\r"`
+NEW_HOSTNAME=$(whiptail --inputbox "Please enter a hostname" 20 60 "$CURRENT_HOSTNAME" 3>&1 1>&2 2>&3)
+if [ $? -eq 0 ]; then
+  echo $NEW_HOSTNAME | sudo tee /etc/hostname
+  sudo sed -i "s/127.0.1.1.*$CURRENT_HOSTNAME/127.0.1.1\t$NEW_HOSTNAME/g" /etc/hosts
+fi
+
+sudo apt-get update
+sudo apt-get --yes install git vim
+
+git clone https://github.com/matthewrkitson/infopanel.git
+
+if ! crontab -l | grep infopanel/software/launch.sh; then
+  crontab -l | sed '$ a @reboot infopanel/software/launch.sh' | crontab
+fi
+
+sudo raspi-config --expand-rootfs
+whiptail --msgbox "Now please reboot the system" 20 60 2
